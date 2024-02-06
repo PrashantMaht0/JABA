b7365c9edcc0e55e36858a6d436bfd21f81fbd55
_Prax_Programmer <prashantmahto111@gmail.com>
2024-02-06 11:59:26 +0530

    minor changes

--- typeCasting.java
+++ typeCasting.java
@@ -8,11 +8,11 @@ public class typeCasting {
 	  short myShort = (short) myLongInt; // Narrowing Casting (long int -> short int)
     //check if all have the same vaule i.e 9
     System.out.println("myInt = "+myInt);
-    System.out.println("myDouble = "+myLongInt);
+    System.out.println("myLong = "+myLongInt);
     System.out.println("myShort = "+myShort);
     //cheking there data types weather or not the there types are being casted.
     System.out.println("Data Type of myInt = "+((Object)myInt).getClass().getSimpleName());
-    System.out.println("Data Type of myDouble = "+((Object)myLongInt).getClass().getSimpleName());
+    System.out.println("Data Type of myLong = "+((Object)myLongInt).getClass().getSimpleName());
     System.out.println("Data Type of myShort = "+((Object)myShort).getClass().getSimpleName());
   }
 }
