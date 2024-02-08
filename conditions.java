b6e5ae70c853d0ea406ff4406fa78938b60797d3
_Prax_Programmer <prashantmahto111@gmail.com>
2024-02-08 14:31:45 +0530

    Added if ladder and alternate(better) me

--- conditions.java
+++ conditions.java
@@ -18,10 +18,15 @@ public class conditions {
       System.out.println("Good evening.");
     }
 
+
+    System.out.println("-----------------------------------------------------------------------------------");
+
     
     //Short hand if-else (Using Ternary Operator(?)):
     String result = (time < 18) ? "Good day." : "Good evening.";
     System.out.println(result);
+
+    System.out.println("-----------------------------------------------------------------------------------");
 
     //Switch Statement:
 
@@ -48,8 +53,43 @@ public class conditions {
         case 7:
           System.out.println("Sunday");
           break;
-}
+      }
+      System.out.println("-----------------------------------------------------------------------------------");
+      
+      //use of if ladder :
+      //the if ladder can be used to add multiple conditions inside a single if statement :
+      //for example here we chcek if number is even and is it between 20 - 40 or not 
+      int num2 = 20;
+      int check = num2&1 ;
+      if(check == 0){
+        System.out.println("number is even :pass ");
 
+        if(num2 < 40 ){
+          System.out.println("Number is less than 40 : pass");
+          
+        }
+        if(num2 >= 20){
+          System.out.println("Number is greater than 20 : pass");
+          
+        }
+        
+      }
+      else {
+        System.out.println("Not a vaid number");
+      }
+      
+      System.out.println("-----------------------------------------------------------------------------------");
+      
+      //another method the add numltiple condtion and perform different operations and manpulations using logical operators :
+      //logical And( && ): true only if all the conditions are true .
+
+      if(check == 0 &&  num2 <= 40 && num2 >= 20 ){
+        System.out.println("Number is even and between the range of 20-40 .");
+
+      }
+
+      //Logical OR (||): True if any one of the conditions are true.
+      //logical NOt (!): Negates or Resverse the condition .
 
     }
     
