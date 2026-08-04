a7e8dae4ae87f1a72f0c96b23380c8eb8da13dbd
Prashant Mahto <119067260+PrashantMaht0@users.noreply.github.com>
2026-08-04 15:47:46 +0100

    Testing the SOP Generation

---
+++ test8.java
@@ -0,0 +1,6 @@
+//testing new features of java25
+public class test8 {
+    public void main(){
+        System.out.println("Now you dont have to write public static void main only public void main or void main will work");
+    }
+}
